c654e188f0785e38f6f972eed87ad11c3f967ea3
Musango <musangow@gmail.com>
2019-01-07 13:36:06 +0200

    Added blog page

---
+++ src/pages/my-blog/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+import PropTypes from "prop-types";
+
+const MyBlog = props => {
+  return (
+    <Layout>
+      <div></div>
+    </Layout>
+  )
+};
+
+MyBlog.propTypes = {};
+MyBlog.defaultProps = {};
+
+export default MyBlog;
