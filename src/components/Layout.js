526afc7d24c580c8d6be9e5f45d1119bd4b515b5
Musango <musangow@gmail.com>
2019-01-09 19:18:11 +0200

    Removed neccesarry tags

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -37,30 +37,6 @@ const TemplateWrapper = ({ children, pathName = null }) => {
               name="description"
               content={data.site.siteMetadata.description}
             />
-
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href="/img/apple-touch-icon.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              href="/img/favicon-32x32.png"
-              sizes="32x32"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              href="/img/favicon-16x16.png"
-              sizes="16x16"
-            />
-
-            <link
-              rel="mask-icon"
-              href="/img/safari-pinned-tab.svg"
-              color="#ff4400"
-            />
             <meta name="theme-color" content="#fff" />
             <meta name="keywords" content={data.site.siteMetadata.keywords} />
             <meta
