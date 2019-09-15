d7fa36c925ed888bd83a62f1f1299ab754f4fd18
Musango <musangow@gmail.com>
2019-09-15 23:06:40 +0200

    Update of packages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,9 @@
 import CMS from 'netlify-cms'
+import uploadcare from "netlify-cms-media-library-uploadcare";
+import cloudinary from "netlify-cms-media-library-cloudinary";
+
+CMS.registerMediaLibrary(uploadcare);
+CMS.registerMediaLibrary(cloudinary);
 
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 
