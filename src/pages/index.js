3eb461f85a9fcc7250e107b698b93ab56a8e585a
Musango <musangow@gmail.com>
2022-01-19 00:00:56 +0200

    remove design page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React, { Fragment } from "react"
 import { Router } from "@reach/router"
 import BlogPage from "./blog"
 import HomePage from "./home"
-import DesignPage from "./design"
 import AboutPage from "./about"
 import WebDevPage from "./web-development"
 import SameStartPage from "./same-start"
@@ -14,7 +13,6 @@ const IndexPage = () => {
       <Router>
         <HomePage path="/" />
         <BlogPage path="blog" />
-        <DesignPage path="design" />
         <AboutPage path="about" />
         <WebDevPage path="web-development" />
         <SameStartPage path="same-start" />
