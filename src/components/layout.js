2602970204aeedbe93456beef3f6da11a1302f88
Musango <musangow@gmail.com>
2022-01-18 22:43:24 +0200

    Update Layout and Seo components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,50 +1,190 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import Navbar from "./Navbar"
+import styled from "styled-components"
+import themed from "../functions/themed"
+import PrimaryButtonLink from "../elements/PrimaryButtonLink"
+import ChevronRightIcon from "../svgs/chevron-right.inline.svg"
+import ChevronLeftIcon from "../svgs/chevron-left.inline.svg"
+import "../styles/global.css"
 
-import Header from "./header"
+const IconWrapper = styled.div`
+  margin-left: 10px;
+  margin-right: 10px;
+  display: flex;
+  align-items: center;
+  svg {
+    width: 25px;
+    height: auto;
+    path {
+      transition: fill 0.3s ease-in-out;
+    }
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
+    @media screen and (max-width: ${props => props.theme.breakpoints.sm}) {
+      width: 15px;
+    }
+  }
+`
+
+const LinkText = styled.span`
+  width: 0;
+  overflow: hidden;
+  transition: width 200ms ease-in-out;
+  display: flex;
+`
+
+const NavBtnLink = styled(PrimaryButtonLink)`
+  height: 74px;
+  background-color: ${props => props.theme.colors.white};
+  position: fixed;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 999;
+  display: flex;
+  flex-flow: nowrap row;
+  align-items: center;
+  padding: 10px;
+  justify-content: center;
+
+  @media screen and (max-width: ${props => props.theme.breakpoints.sm}) {
+    height: 50px;
+    width: 40px;
+  }
+
+  &:hover {
+    ${LinkText} {
+      width: 200px;
+    }
+    svg {
+      path {
+        fill: ${props => props.theme.colors.white};
       }
     }
-  `)
+  }
+`
+
+const PrevNavBtnLink = styled(NavBtnLink)`
+  left: 0;
+  flex-direction: row-reverse;
+`
+
+const NextNavBtnLink = styled(NavBtnLink)`
+  right: 0;
+`
+
+const StyledLayout = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Navigator = ({ path }) => {
+  const getFirstButtonLink = () => {
+    switch (path) {
+      case "/about/":
+        return {
+          path: "",
+          linkText: "",
+        }
+      case "/web-development/":
+        return {
+          path: "/about",
+          linkText: "About",
+        }
+      case "/design/":
+        return {
+          path: "/web-development",
+          linkText: "Web Dev",
+        }
+      case "/blog/":
+        return {
+          path: "/design",
+          linkText: "Design",
+        }
+      default:
+        return {}
+    }
+  }
+
+  const getLastButtonLink = () => {
+    switch (path) {
+      case "/about/":
+        return {
+          path: "/web-development",
+          linkText: "Web Dev",
+        }
+      case "/web-development/":
+        return {
+          path: "/design",
+          linkText: "Design",
+        }
+      case "/design/":
+        return {
+          path: "/blog",
+          linkText: "Blog",
+        }
+      case "/blog/":
+        return {
+          path: "",
+          linkText: "",
+        }
+      default:
+        return {}
+    }
+  }
+
+  const getNavBtn = (
+    getButtonMethod = () => {},
+    linkComponent = null,
+    icon = React.Fragment
+  ) => {
+    const { path = "", linkText = "" } = getButtonMethod()
+    const LinkComponent = linkComponent
+    const Icon = icon
+    if (path && linkText) {
+      return (
+        <LinkComponent to={path}>
+          <LinkText>{linkText}</LinkText>
+          <IconWrapper>
+            <Icon />
+          </IconWrapper>
+        </LinkComponent>
+      )
+    }
+    return null
+  }
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <React.Fragment>
+      {getNavBtn(getFirstButtonLink, PrevNavBtnLink, ChevronLeftIcon)}
+      {getNavBtn(getLastButtonLink, NextNavBtnLink, ChevronRightIcon)}
+    </React.Fragment>
+  )
+}
+
+Navigator.propTypes = {
+  path: PropTypes.string,
+}
+
+Navigator.defaultProps = {
+  path: "",
+}
+
+const Layout = ({ children, path }) => {
+  return (
+    <StyledLayout>
+      <Navbar />
+      <Navigator path={path} />
+      {children}
+    </StyledLayout>
   )
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  path: PropTypes.string,
+}
+Layout.defaultProps = {
+  path: "",
 }
 
-export default Layout
+export default themed(Layout)
--- src/components/seo.js
+++ src/components/seo.js
@@ -7,10 +7,10 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
+import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+function Seo({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -52,6 +52,10 @@ function SEO({ description, lang, meta, title }) {
           content: `website`,
         },
         {
+          property: `og:image`,
+          content: `src/images/og-image.jpg`,
+        },
+        {
           name: `twitter:card`,
           content: `summary`,
         },
@@ -72,17 +76,17 @@ function SEO({ description, lang, meta, title }) {
   )
 }
 
-SEO.defaultProps = {
+Seo.defaultProps = {
   lang: `en`,
   meta: [],
   description: ``,
 }
 
-SEO.propTypes = {
+Seo.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
 }
 
-export default SEO
+export default Seo
